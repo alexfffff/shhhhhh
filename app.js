e921750623638a0fb418b27faf85d5facea65502
dluisnothere <43430369+dluisnothere@users.noreply.github.com>
2020-12-03 14:11:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var routes = require('./routes/routes.js');
 const path = require('path');
 var app = express();
 app.use(express.urlencoded());
-//app.use(express.static('public'))
 app.use(express.static('public'));
 // handle sessions and cookies for users
 var session = require('express-session');
