c6b5887b84f79139124212b4927d615201b76786
alexfffff <59675371+alexfffff@users.noreply.github.com>
2020-12-18 01:52:21 -0600

    Update model.js

--- model.js
+++ model.js
@@ -1,36 +1,8 @@
-var AWS = require("aws-sdk");
-
-AWS.config.update({
-  region: "us-west-2",
-  endpoint: "http://localhost:8000"
-});
-
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
 var dynamodb = new AWS.DynamoDB();
 
 
-var params = {
-    TableName : "Movies",
-    KeySchema: [       
-        { AttributeName: "year", KeyType: "HASH"},  //Partition key
-        { AttributeName: "title", KeyType: "RANGE" }  //Sort key
-    ],
-    AttributeDefinitions: [       
-        { AttributeName: "year", AttributeType: "N" },
-        { AttributeName: "title", AttributeType: "S" }
-    ],
-    ProvisionedThroughput: {       
-        ReadCapacityUnits: 10, 
-        WriteCapacityUnits: 10
-    }
-};
-
-dynamodb.createTable(params, function(err, data) {
-    if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-    }
-});
 
 var params = {
     TableName : "users",
@@ -138,6 +110,10 @@ var params = {
             KeySchema: [
                 { AttributeName: 'postID', KeyType: 'HASH' }, 
                 { AttributeName: 'timestamp', KeyType: 'RANGE' }
+            ],
+            AttributeDefinitions: [       
+                { AttributeName: "postID", AttributeType: "S" },
+                { AttributeName: "timestamp", AttributeType: "S" }
             ]
         }
     ],
@@ -170,6 +146,9 @@ var params = {
             IndexName: 'username-index', 
             KeySchema: [
                 { AttributeName: 'username', KeyType: 'HASH' }
+            ],
+            AttributeDefinitions: [       
+                { AttributeName: "username", AttributeType: "S" }
             ]
         }
     ],
@@ -200,6 +179,9 @@ var params = {
             IndexName: 'date-index', 
             KeySchema: [
                 { AttributeName: 'date', KeyType: 'HASH' }
+            ],
+            AttributeDefinitions: [       
+                { AttributeName: "date", AttributeType: "S" }
             ]
         }
     ],
@@ -256,6 +238,9 @@ var params = {
             IndexName: 'date-index', 
             KeySchema: [
                 { AttributeName: 'date', KeyType: 'HASH' }
+            ],
+            AttributeDefinitions: [
+                { AttributeName: 'date', AttributeType: 'S' }
             ]
         }
     ],
